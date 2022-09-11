01a3dadf9a55b7f07c26a84ad7be63b6bb5b47eb
Yahye <yahyemoh2002@gmail.com>
2022-09-11 15:26:23 -0400

    Worked on the navigation bar and currently I am fixing the dropdown nav

---
+++ script.js
@@ -0,0 +1,13 @@
+const menu_icon = document.getElementById('menu');
+const bar_one = document.getElementsByClassName('bar1');
+const bar_two = document.getElementsByClassName('bar2');
+const bar_three = document.getElementsByClassName('bar3');
+const dropdown = document.getElementsByClassName('bot-nav');
+
+function showMenu() {
+    menu_icon.classList.toggle('change');
+    dropdown.classList.toggle('dropdown');
+    dropdown.classList.toggle('dont-dropdown');
+}
+
+menu_icon.addEventListener('click', showMenu)
