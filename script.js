b00906efeddd2866fa1618e7bd53cf557a9ae281
Yahye <yahyemoh2002@gmail.com>
2022-09-11 17:40:36 -0400

    Fixed the problem with the dropdown manu

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const menu_icon = document.getElementById('menu');
 const bar_one = document.getElementsByClassName('bar1');
 const bar_two = document.getElementsByClassName('bar2');
 const bar_three = document.getElementsByClassName('bar3');
-const dropdown = document.getElementsByClassName('bot-nav');
+const dropdown = document.querySelector('div.bot-nav');
 
 function showMenu() {
     menu_icon.classList.toggle('change');
-    dropdown.classList.toggle('dropdown');
     dropdown.classList.toggle('dont-dropdown');
 }
 
